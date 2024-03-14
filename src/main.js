bd448e562fd4c6f5d626dba5bd1832aa5062abaf
Traunin <traunin5@gmail.com>
2024-03-14 21:47:16 +0300

    Change paths for webpack

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 import "reset-css";
 import "@/assets/style/style.css";
-import router from "./routes";
-import $bus from "./utils/Events";
+import router from "@/routes";
 
 const app = createApp(App);
 
 app.use(router);
-
-app.config.globalProperties.$bus = $bus;
 
 app.mount("#app");
