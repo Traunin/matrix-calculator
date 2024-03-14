3ada674fe5482fd2bb8c866d133d55bc13520c97
Traunin <traunin5@gmail.com>
2024-03-14 21:52:52 +0300

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import DeterminantCalculator from "./components/DeterminantCalculator.vue"
-import MatrixSolver from "./components/MatrixSolver.vue"
-import MatrixMultiplier from "./components/MatrixMultiplier.vue"
-
-
+import DeterminantCalculator from "@/views/DeterminantCalculator.vue";
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         { path: "/", component: DeterminantCalculator },
         { path: "/determinant", component: DeterminantCalculator },
-        { path: "/roots", component: MatrixSolver },
-        { path: "/multiply", component: MatrixMultiplier },
     ],
 });
 
