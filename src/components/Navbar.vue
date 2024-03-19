e0c3de8b2248e12f7034ad50dcf3ccf172fae51a
Traunin <traunin5@gmail.com>
2024-03-19 22:11:14 +0300

    Refactor links to use router-link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,12 +2,9 @@
     <nav>
         <ul>
             <navbar-link
-                v-for="(link, index) in pages"
+                v-for="link in pages"
                 :name="link.name"
                 :link="link.link"
-                :index="index"
-                :isActive="activePage == index"
-                @click="setActivePage(index)"
             ></navbar-link>
         </ul>
     </nav>
@@ -21,27 +18,15 @@ export default {
         NavbarLink,
     },
 
-    mounted() {
-        let activePage = sessionStorage.getItem("activePage");
-        this.activePage = activePage == null ? 0 : activePage;
-    },
-
     data() {
         return {
-            activePage: 0,
             pages: [
                 { name: "Рассчет определителя рекурсией", link: "determinant" },
                 { name: "Найти корни системы методом Крамера", link: "solver" },
+                { name: "Умножить матрицы", link: "multiplier" },
             ],
         };
     },
-
-    methods: {
-        setActivePage(index) {
-            this.activePage = index;
-            sessionStorage.setItem('activePage', index);
-        }
-    }
 };
 </script>
 
