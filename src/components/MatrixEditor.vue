1e0eef7afc7129fe661c1fa766ae78058a2ca54f
Traunin <traunin5@gmail.com>
2024-03-24 16:32:43 +0300

    Add space to list of possible characters

--- src/components/MatrixEditor.vue
+++ src/components/MatrixEditor.vue
@@ -191,7 +191,12 @@ function closeEditor() {
 function isNumber(evt) {
     //evt = evt ? evt : window.event;
     var charCode = evt.which ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46) {
+    if (
+        charCode > 31 &&
+        (charCode < 48 || charCode > 57) &&
+        charCode !== 46 &&
+        charCode !== 45
+    ) {
         evt.preventDefault();
     } else {
         return true;
